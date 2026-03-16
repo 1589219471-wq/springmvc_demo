8f8e9c0c446733eb36c97abe71983273259cd962
0zy0 <1589219471@qq.com>
2026-03-16 22:23:35 +0800

    ，

---
+++ springboot-web-rep-resp/src/main/java/com/itheima/controller/RequestController.java
@@ -0,0 +1,36 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.User;
+import com.sun.net.httpserver.HttpServer;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*@RestController
+public class RequestController {
+
+    @RequestMapping("/simpleParam")
+    public String simpleParam(HttpServletRequest request){
+        String name=request.getParameter( "name" );
+        int age = Integer.parseInt( request.getParameter( "age" ) );
+        System.out.println(name+":"+age);
+        return "OK";
+    }
+}*/
+@RestController
+public class RequestController {
+
+    @RequestMapping("/simpleParam")
+    public String simpleParam(String name,Integer age){
+        System.out.println(name+":"+age);
+
+        return "OK";
+    }
+
+
+    @RequestMapping("/simplePojo")
+    public String simplePojo(User user){
+        System.out.println(user);
+        return "OK";
+    }
+}
